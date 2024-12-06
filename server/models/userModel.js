877cd6bdd5f3d0c1582fa6ead483ee0fd72e5add
Pawanjhajha <pawankumarjhajharia3@gmail.com>
2024-12-06 17:57:21 +0530

    create the user schema

---
+++ server/models/userModel.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const userSchema=new mongoose.Schema({
+    firstName:{
+        type:String,
+        required:true,
+    },
+    lastName:{
+        type:String,
+        required:true,
+    },
+    email:{
+        type:String,
+        required:true,
+    },
+    password:{
+        type:String,
+        required:true,
+    },
+    profilePic:{
+        type:String,
+        required:false,
+    }
+
+},{timestamps:true});
+//schema basically tell what property we need to create the user object
+export const userModel=mongoose.model('core_users',userSchema);//it will take two perameter and 1.model name 2.userScheam
+//a model is like the blue print based on that mongoose create the document.mongodb create take model for create the document
